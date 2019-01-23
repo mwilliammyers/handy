4cf7b9d4e3a52e68bce177f25161f2d2cb6d60eb
William Myers <mwilliammyers@gmail.com>
2019-01-23 10:44:33 -0700

    Update rsync.fish

--- functions/rsync.fish
+++ functions/rsync.fish
@@ -1,3 +1,8 @@
 function rsync --description rsync
-	command  rsync -ahuL --partial --info=progress2 --exclude='.DS_Store' $argv
+    set exclude ''
+    if test -e $PWD/.gitignore
+        set exclude "--exclude-from=.gitignore"
+    end
+
+    command  rsync -ahuL --partial --info=progress2 --exclude=.git --exclude='.DS_Store' $exclude $argv
 end
