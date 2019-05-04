d0c2352fbaa7d4de808341f73140cedcba580085
William Myers <mwilliammyers@gmail.com>
2019-05-04 12:36:31 -0600

    Fix argv handling in rsync.fish

--- functions/rsync.fish
+++ functions/rsync.fish
@@ -1,14 +1,14 @@
 function rsync --description rsync
     set exclude '.DS_Store'
-    for arg in "$argv"
-        if test -d "$arg"
-            set result (git -C "$arg" ls-files --ignore --exclude-standard --others --directory ^/dev/null)
+    for arg in $argv
+        if test -d $arg
+            set result (git -C $arg ls-files --ignore --exclude-standard --others --directory ^/dev/null)
             set exclude "$exclude "$result
             # the source directory will always be before the destination
             break
         end
     end
-    
+
     # TODO: add --protect-args option?
     # TODO: add --fake-super option?
     command  rsync \
