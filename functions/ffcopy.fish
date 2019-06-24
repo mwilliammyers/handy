5f9771689a7e1d430012f394ded5147b5556227a
William Myers <mwilliammyers@gmail.com>
2019-06-23 22:23:34 -0600

    Update ffcopy.fish

--- functions/ffcopy.fish
+++ functions/ffcopy.fish
@@ -6,8 +6,9 @@ function ffcopy
         # TODO: be smarter about this
         set -l ext (string split '.' "$name")[-1]
        
+        # TODO: should we transcode to mp4 at the same time, or leave it as is?
         set -l out_path (string replace "$ext" "mp4" "$path")
     
-        ffmpeg -i "$path" -c:v copy -c:a copy "$out_path"
+        ffmpeg -i "$path" -c:v copy -c:a copy -c:s copy -c:d copy -c:t copy "$out_path"
     end
 end
