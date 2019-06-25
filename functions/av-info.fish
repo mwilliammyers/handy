c289fe3f8b7afeb2f0fb8d7b091a302a75c0dfc0
William Myers <mwilliammyers@gmail.com>
2019-06-24 18:30:11 -0600

    Delete av-info.fish

--- functions/av-info.fish
+++
@@ -1,9 +0,0 @@
-function av-info
-    command ffprobe \
-        -v error \
-        -print_format json \
-        -hide_banner \
-        -show_entries stream=codec_name,height,width:format= \
-        $argv \
-        | jq .streams
-end
