cefa81638f4ea6db185c3912d12ba114bb6199ab
William Myers <mwilliammyers@gmail.com>
2019-08-16 14:11:44 -0600

    Update fzf.fish

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -1,4 +1,4 @@
 if type -q 'fd'
-	set -l exclude "-E .git/ -E target/ -E .Trash/ -E .trash/ -E '*Cache*/' -E '*cache*/' -E Applications/ -E Library/ -E 'Album Artwork/' -E .rustup/ -E registry/ -E .npm/ -E node_modules/ -E .node-gyp/ -E venv/ -E env/ -E .apm/ -E packages/ -E pack/ -E '*.xcodeproj/'"
+	set -l exclude "-E .git/ -E '*target/ -E .Trash/ -E .trash/ -E '*Cache*/' -E '*cache*/' -E Applications/ -E Library/ -E 'Album Artwork/' -E .rustup/ -E registry/ -E .npm/ -E node_modules/ -E .node-gyp/ -E venv/ -E env/ -E .apm/ -E packages/ -E pack/ -E '*.xcodeproj/'"
 	set -q FZF_DEFAULT_COMMAND; or set -U FZF_DEFAULT_COMMAND "fd -t f $exclude"
 end
