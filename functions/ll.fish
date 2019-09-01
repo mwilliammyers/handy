f965609f08f70a6a545a3a1b694a962cd7324ad5
William Myers <mwilliammyers@gmail.com>
2019-09-01 14:06:44 -0600

    Update ll.fish

--- functions/ll.fish
+++ functions/ll.fish
@@ -1,4 +1,4 @@
 function ll --description "List contents of directory using long format"
-	exa --long --group --extended --modified --git --time-style=iso $argv
+	exa --long --group --modified --git --time-style=iso $argv
 end
 
