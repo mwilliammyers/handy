749331b509e63a5a8d6342ca1d9fe4a3ac2ea5cd
William Myers <mwilliammyers@gmail.com>
2019-10-05 13:35:35 -0700

    Update cleanup.fish

--- functions/cleanup.fish
+++ functions/cleanup.fish
@@ -1,12 +1,15 @@
 function cleanup -d "cleanup unecessary files & directories"
-	rm -rfi ~/.DS_Store ~/.subversion/ ~/.lesshst ~/.ansible .ansible_galaxy \
+	rm -rfi ~/.subversion/ ~/.lesshst ~/.ansible .ansible_galaxy \
 		~/.sh_history ~/.bash_profile ~/.bashrc ~/.bash_history ~/.zshrc \
 		~/.vim ~/.viminfo ~/.rnd ~/.node-gyp ~/.node_repl_history \
 		~/.python_history ~/.tox ~/.ipython ~/.pylint.d \
 		~/.atom ~/.gi_list ~/.matplotlib ~/.ipython ~/.jupyter ~/.python27_compiled \
 		~/.sync-settings.cache ~/.babel.json ~/.ipynb_checkpoints ~/.cups \
-		~/.ansible_galaxy ~/.serverless/ ~/.gsutil/ ~/.plotly/ ~/.cufflinks/
-
+		~/.ansible_galaxy ~/.serverless/ ~/.plotly/ ~/.cufflinks/
+	
+	# TODO: figure out how to get gsutil to move these somewhere else
+	rm -rf ~/.DS_Store ~/.boto ~/.gsutil/
+	
 	# get around fish complaining about empty shell glob matches
 	for f in ~/.v8flags*
 		rm -fi $f
