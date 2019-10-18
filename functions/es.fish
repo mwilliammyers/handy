2cac1b4c817e8c7e0affd7efb678578e92bffa40
William Myers <mwilliammyers@gmail.com>
2019-10-18 17:50:48 -0600

    Update es.fish

--- functions/es.fish
+++ functions/es.fish
@@ -2,10 +2,11 @@ function es --description "Query Elasticsearch" --argument url
     set -l db_url "$ES_URL"
 
     test -n "$db_url"
-    or set -l db_url "http://localhost:9200"
+    or set -l db_url "http://127.0.0.1:9200"
 
     command curl -sS \
         -H 'Content-Type: application/json' \
+        -H 'Accept: application/json' \
         "$db_url/$url" \
         $argv[2..-1] \
         | jq
