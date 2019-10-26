5ae5b5dc9ea30ee095032f6d04e1c6ce35d381c6
William Myers <mwilliammyers@gmail.com>
2019-10-26 14:57:46 -0600

    Update pwgen.fish
    Enable symbols by default (API is no longer compatible with pwgen).

--- functions/pwgen.fish
+++ functions/pwgen.fish
@@ -3,21 +3,20 @@ function pwgen -d "Generate a secure password"
     set default_pw_length 20
     set default_num_pw 1
     set default_allowed_chars 'a-zA-Z0-9'
-    set symbols '@#$%^&*()_+{}|:<>?=' # or just @$!%*#?&' ?
+    set default_symbols '@#$%^&*()_+{}|:<>?=' # or just @$!%*#?&' ?
     set min_block_size 128
     set num_blocks_scale 2.1 # empirical rough estimate
     set block_size_scale 1.3 # empirical rough estimate
 
-    argparse --name=pwgen 'h/help' 'N/num-passwords=!_validate_int --min 1' 'y/symbols' 'c/allowed-chars=' -- $argv
+    argparse --name=pwgen 'h/help' 'N/num-passwords=!_validate_int --min 1' 's/symbols=' -- $argv
     or return
 
     if set -q _flag_help
         printf "Usage: pwgen [OPTIONS] [PW_LENGTH]\n\n"
         printf "Options:\n"
-        printf "  -h/--help                  Prints help and exits.\n"
-        printf "  -N/--num-passwords=NUM     Generate [NUM] passwords (default: $default_pw_length).\n"
-        printf "  -y/--symbols               Include at least one special character in the password.\n"
-        printf "  -c/--allowed-chars=STRING  Only include specified characters (default: $default_allowed_chars)."
+        printf "  -h/--help               Prints help and exits.\n"
+        printf "  -N/--num-passwords=NUM  Generate [NUM] passwords (default: $default_pw_length).\n"
+        echo   "  -s/--symbols=STRING     Include specified symbols in addition to alphanumeric characters (default: $default_symbols)."
 
         return 0
     end
@@ -34,22 +33,17 @@ function pwgen -d "Generate a secure password"
     test -z $num_pw
     and set num_pw $default_num_pw
 
-    if set -q _flag_c
-        set allowed_chars $_flag_c
-    else
-        set allowed_chars $default_allowed_chars
-
-        set -q _flag_y
-        and set allowed_chars $symbols$allowed_chars
-    end
+	set symbols $_flag_s
+	test -z $symbols
+	and set symbols $default_symbols
+	# TODO: allow setting characters explicitly?
+	set allowed_chars $symbols$default_allowed_chars
 
     set num_blocks (math "round($num_blocks_scale * $num_pw)")
 
     set block_size (math "round($block_size_scale * $pw_length)")
-
     test $block_size -lt $min_block_size
     and set block_size $min_block_size
-
     # nearest power of 2
     set block_size (math "pow(2, ceil(log($block_size) / log(2)))")
 
